e2f71864a9839aa67aa706013073ed4a215e5811
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-06-20 13:33:13 +0530

    adding the python file

---
+++ image to text.py
@@ -0,0 +1,23 @@
+from pywhatkit import image_to_ascii_art as ch
+
+img1 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\doremon.jpg'
+img2 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\ironman.jpg'
+img3 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\pikachu.png'
+img4 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\rdjr.jpg'
+img5 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\shinchan.png' 
+
+
+t1 = 't1.txt'
+t2 = 't2.txt'
+t3 = 't3.txt'
+t4 = 't4.txt'
+t5 = 't5.txt'
+
+
+ch(img1, t1)
+ch(img2, t2)
+ch(img3, t3)
+ch(img4, t4)
+ch(img5, t5)
+
+print('completed...')
