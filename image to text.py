970f7f5f4804ffd24f0ac05d873a85da6889a232
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-06-20 13:51:38 +0530

    adding commands

--- image to text.py
+++ image to text.py
@@ -1,10 +1,12 @@
-from pywhatkit import image_to_ascii_art as ch
+from pywhatkit import image_to_ascii_art as ch        #imprting the function from pywhatkit as ch
 
-img1 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\doremon.jpg'
-img2 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\ironman.jpg'
-img3 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\pikachu.png'
-img4 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\rdjr.jpg'
-img5 = 'C:\\Users\\kamat\\Desktop\\python projrcts\\yt\\v1\\explain\\shinchan.png' 
+# if it shows any error then enter the full path to the file in the string (eg: img1 = 'c:\\user\\Desktop\\doremo.jpg)
+
+img1 = 'doremon.jpg'  
+img2 = 'ironman.jpg'
+img3 = 'pikachu.png'
+img4 = 'rdjr.jpg'
+img5 = 'shinchan.png' 
 
 
 t1 = 't1.txt'
@@ -13,11 +15,14 @@ t3 = 't3.txt'
 t4 = 't4.txt'
 t5 = 't5.txt'
 
+#converting images into text
 
-ch(img1, t1)
+ch(img1, t1)  
 ch(img2, t2)
 ch(img3, t3)
 ch(img4, t4)
 ch(img5, t5)
+
+#printing completed for our conformation..
 
 print('completed...')
